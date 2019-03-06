ffd16de421b9df56446421070f7707beb41c076f
Fabio Righi <fabio@onestic.com>
2019-03-06 10:39:59 +0100

    Add command description and shell task for frontools

---
+++ console/commands/gulp.sh
@@ -0,0 +1,19 @@
+#!/usr/bin/env bash
+set -euo pipefail
+
+usage()
+{
+    printf "${YELLOW}Usage:${COLOR_RESET}\n"
+    echo "  gulp command"
+    echo ""
+    echo "Example:"
+    printf "  ${GREEN}dockergento gulp style ${COLOR_RESET}\n"
+}
+
+if [ "$#" == 0 ] || [ "$1" == "--help" ]; then
+    usage
+    exit 0
+fi
+
+${DOCKER_COMPOSE} run --rm --service-ports ${SERVICE_NODE} sh -c "cd ${MAGENTO_DIR}/frontools && \
+    npx gulp $@"
