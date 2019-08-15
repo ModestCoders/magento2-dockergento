4390c4cdfc0ad09e82763b6113bbb7f429a2be4c
Ihor Sviziev <ihor-sviziev@users.noreply.github.com>
2019-08-15 11:59:41 +0300

    Speed up debug on and off
    Restart only php container after changing xdebug settings

--- console/commands/debug-off.sh
+++ console/commands/debug-off.sh
@@ -5,7 +5,6 @@ ${TASKS_DIR}/start_service_if_not_running.sh ${SERVICE_PHP}
 
 ${COMMANDS_DIR}/exec.sh sed -i -e 's/^\zend_extension/;zend_extension/g' /usr/local/etc/php/conf.d/docker-php-ext-xdebug.ini
 
-${COMMANDS_DIR}/stop.sh
-${COMMANDS_DIR}/start.sh
+${DOCKER_COMPOSE} restart ${SERVICE_PHP}
 
 printf "> ${GREEN}xdebug disabled${COLOR_RESET}\n"
